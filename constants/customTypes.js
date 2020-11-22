6effb67c95b88a4454a383c2ce9a7a85e08df402
qalqi <qalqi@qalqi.com>
2020-11-22 11:02:57 +0530

    Compiled es6 customTypes

--- constants/customTypes.js
+++ constants/customTypes.js
@@ -1,37 +1,44 @@
-const customTypes = {
-    DelegationNodeId: 'Hash',
-    PublicSigningKey: 'Hash',
-    PublicBoxKey: 'Hash',
-    Permissions: 'u32',
-    ErrorCode: 'u16',
-    "Attribute": {
-        "name": "Vec<u8>",
-        "value": "Vec<u8>",
-        "validity": "BlockNumber",
-        "creation": "Moment",
-        "nonce": "u64"
-    },
-    "AttributeTransaction": {
-        "signature": "Signature",
-        "name": "Vec<u8>",
-        "value": "Vec<u8>",
-        "validity": "u32",
-        "signer": "AccountId",
-        "identity": "AccountId"
-    },
-    "TokenId": "u64",
-    "SwapId": "u64",
-    "TokenBalance": "u64",
-    "MissionTokenId": "u32",
-    "RegistrarIndex": "u32",
-    "Judgement": {
-        "_enum": ["Requested", "Approved"]
-    },
-    "JudgementItem": "(RegistrarIndex, Judgement)",
-    "Registration": {
-        "judgements": "Vec<JudgementItem>",
-        "account_id": "AccountId"
-    }
-};
+"use strict";
 
-export default customTypes;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = void 0;
+const customTypes = {
+  DelegationNodeId: "Hash",
+  PublicSigningKey: "Hash",
+  PublicBoxKey: "Hash",
+  Permissions: "u32",
+  ErrorCode: "u16",
+  Attribute: {
+    name: "Vec<u8>",
+    value: "Vec<u8>",
+    validity: "BlockNumber",
+    creation: "Moment",
+    nonce: "u64"
+  },
+  AttributeTransaction: {
+    signature: "Signature",
+    name: "Vec<u8>",
+    value: "Vec<u8>",
+    validity: "u32",
+    signer: "AccountId",
+    identity: "AccountId"
+  },
+  TokenId: "u64",
+  SwapId: "u64",
+  TokenBalance: "u64",
+  MissionTokenId: "u32",
+  RegistrarIndex: "u32",
+  Judgement: {
+    _enum: ["Requested", "Approved"]
+  },
+  JudgementItem: "(RegistrarIndex, Judgement)",
+  Registration: {
+    judgements: "Vec<JudgementItem>",
+    account_id: "AccountId"
+  }
+};
+var _default = customTypes;
+exports.default = _default;
